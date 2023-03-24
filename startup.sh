ed1d25138c9cda7043be83922848908447a9e429
Paul Forstman <pforstman@gmail.com>
2023-03-24 15:31:39 -0500

    Added startup.sh

---
+++ startup.sh
@@ -0,0 +1,20 @@
+apt update
+
+apt -y install curl
+
+apt-get update
+
+apt-get install gnupg
+
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+
+curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list | tee /etc/apt/sources.list.d/msprod.list
+
+apt-get update
+
+apt --fix-broken install
+
+ACCEPT_EULA=Y apt-get install mssql-tools
+
+ln -sfn /opt/mssql-tools/bin/sqlcmd /usr/bin/sqlcmd
+ln -sfn /opt/mssql-tools/bin/bcp /usr/bin/bcp
